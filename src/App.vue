9cce7af7cce594ee54e2a5e9e3e34262d4fbf8e6
xIgor1982 <i_swat@mail.ru>
2022-10-04 22:23:24 +0300

    Первая часть. Реализация с помощью script setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="app">
+		<nav>
+			<RouterLink to="/">Home</RouterLink> |
+			<RouterLink to="/about">About</RouterLink>
+		</nav>
+		<RouterView />
+	</div>
+</template>
+
+<style scoped>
+  .app {
+    text-align: center ;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="home">
+		<div class="count">
+			{{ count }}
+		</div>
+		<div class="buttons">
+			<button @click="decrementCount">-</button>
+			<button @click="incrementCount">+</button>
+		</div>
+		<hr />
+		<div>
+			Это четное или нечетное число: <strong>{{ oddOrEvent }}</strong>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue';
+
+const count = ref(0);
+
+const incrementCount = () => {
+	count.value++;
+};
+
+const decrementCount = () => {
+	count.value--;
+};
+
+const oddOrEvent = computed(() => {
+	if (count.value % 2 === 0) return 'четное';
+	else return 'нечетное';
+});
+</script>
+
+<style>
+.count {
+	font-size: 60px;
+	margin: 20px;
+}
+
+.buttons button {
+	font-size: 40px;
+	padding: 10px 30px;
+	margin: 5px;
+}
+</style>
